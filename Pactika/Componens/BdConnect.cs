113865647ed83ddde19aee5a78ff23cfbe483b07
faila <faila@DESKTOP-1C8REGN>
2022-12-07 11:15:02 +0300

    Добавьте файлы проекта.

---
+++ Pactika/Componens/BdConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pactika.Componens
+{
+    internal class BdConnect
+    {
+        public static YYPractikaEntities db = new YYPractikaEntities();
+    }
+}
---
+++ Pactika/Componens/Navigation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+using System.Windows.Controls;
+
+namespace Pactika.Componens
+{
+    internal class Navigation
+    {
+        public static Page<Nav> navs = new Page<Nav>();
+        public static MainWindow main;
+        public static User AutoPage;
+        public static bool IsUser;
+        public static void NextPage(Nav nav)
+    }
+}
